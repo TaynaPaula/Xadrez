c95c51365c09c3adb1d6a5bd1866caee7545801a
TaynaPaula <taynapaulacruz@gmail.com>
2024-09-17 22:21:11 -0300

    First Commit

---
+++ Xadrez/TesteXadrez/src/teste/TesteXadrez.java
@@ -0,0 +1,20 @@
+package teste;
+
+import xadrez.Bispo;
+import xadrez.Cavalo;
+import xadrez.Peao;
+import xadrez.Peca;
+
+public class TesteXadrez {
+    public static void main(String[] args)  {
+
+        Peca peao =new Peao();
+        Peca cavalo =new Cavalo();
+        Peca bispo = new Bispo();
+
+        peao.mover();
+        cavalo.mover ();
+        bispo.mover();
+
+    }
+}
---
+++ Xadrez/TesteXadrez/src/xadrez/Peao.java
@@ -0,0 +1,9 @@
+package xadrez;
+
+public class Peao extends Peca {
+    @Override
+    public void mover(){
+        System.out.println("Andar pra frente");
+    }
+
+}
